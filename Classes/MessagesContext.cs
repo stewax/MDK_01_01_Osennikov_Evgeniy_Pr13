75041e7d1af0cf2253a649339e277f81c06fd1c8
Evgeniy <sbravl637@gmail.com>
2025-11-10 11:18:29 +0500

    Реализованные классы реализующие интерфейсы. Osennikov

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Interface_Osennikov.Interfaces;
+using Interface_Osennikov.Models;
+using System.Collections.Generic;
+
+namespace Interface_Osennikov.Classes
+{
+    public class MessagesContext : Messages, IMessages
+    {
+        public static List<Messages> AllMessages;
+
+        public MessagesContext() => All(out AllMessages);
+
+        public MessagesContext(string messageText, DateTime createdAt, int userId)
+            : base(messageText, createdAt, userId) { }
+
+        public MessagesContext(string messageText, DateTime createdAt, int userId, string imageSource)
+            : base(messageText, createdAt, userId, imageSource) { }
+
+        public void All(out List<Messages> messages)
+        {
+            messages = new List<Messages>();
+        }
+
+        public void Delete()
+        {
+            AllMessages.Remove(this);
+        }
+
+        public void Save(bool update = false)
+        {
+            if (update == false)
+            {
+                AllMessages.Add(this);
+                return;
+            }
+        }
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,39 @@
+﻿using Interface_Osennikov.Interfaces;
+using Interface_Osennikov.Models;
+using System.Collections.Generic;
+
+namespace Interface_Osennikov.Classes
+{
+    public class UsersContext : Users, IUsers
+    {
+        public List<Users> AllUsers;
+        public UsersContext() => All(out AllUsers);
+
+        public void All(out List<Users> users)
+        {
+            users = new List<Users>
+            {
+                new Users(1, "Аликина Ольга"),
+                new Users(2, "Бояркин Данил"),
+                new Users(3, "Бурмантов Владислав"),
+                new Users(4, "Дылдин Максим"),
+                new Users(5, "Квдокимов Даниил"),
+                new Users(6, "Костюнин Никита"),
+                new Users(7, "Кучин Данил"),
+                new Users(8, "Мотырев Александр"),
+                new Users(9, "Мухров Далер"),
+                new Users(10, "Ослов Николай"),
+                new Users(11, "Саблин Константин"),
+                new Users(12, "Субботин Валерий"),
+                new Users(13, "Сукрушев Егор"),
+                new Users(14, "Торсунов Даниил"),
+                new Users(15, "Бобров Евгений"),
+                new Users(16, "Мохова Алена"),
+                new Users(17, "Чернокомазов Анвар"),
+                new Users(18, "Чечеткина Ольга"),
+                new Users(19, "Чумачечий Владислав"),
+                new Users(20, "Шиншилов Дима")
+            };
+        }
+    }
+}
