8e89e0f0878859e4e1f0cf92ea23fedff2efaf31
Evgeniy <sbravl637@gmail.com>
2025-11-10 11:01:54 +0500

    Реализованные модели. Osennikov

---
+++ Models/Messages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Osennikov.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string MessageText { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+
+        public string ImageSource { get; set; }
+
+        public Messages()
+        {
+
+        }
+        public Messages(string messageText, DateTime createdAt, int userId)
+        {
+            MessageText = messageText;
+            CreatedAt = createdAt;
+            UserId = userId;
+            ImageSource = null;
+        }
+        public Messages(string messageText, DateTime createdAt, int userId, string imageSource) : this(messageText, createdAt, userId)
+        {
+            ImageSource = imageSource;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Osennikov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Users()
+        {
+
+        }
+        public Users(int id, string fio)
+        {
+            Id = id;
+            FIO = fio;
+        }
+    }
+}
