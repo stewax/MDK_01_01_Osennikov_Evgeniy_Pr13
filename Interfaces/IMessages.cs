585035f3bf04f75cc38f2d590095e51f8a78fa1b
Evgeniy <sbravl637@gmail.com>
2025-11-10 11:07:30 +0500

    Реализованные интерфейсы. Osennikov

---
+++ Interfaces/IMessages.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Interface_Osennikov.Models;
+
+namespace Interface_Osennikov.Interfaces
+{
+    internal interface IMessages
+    {
+        void All(out List<Messages> messages);
+        void Save(bool update = false);
+        void Delete();
+    }
+}
---
+++ Interfaces/IUsers.cs
@@ -0,0 +1,10 @@
+﻿using Interface_Osennikov.Models;
+using System.Collections.Generic;
+
+namespace Interface_Osennikov.Interfaces
+{
+    internal interface IUsers
+    {
+        void All(out List<Users> users);
+    }
+}
